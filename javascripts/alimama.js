2205ed3bfb52158643fe2182433f1d7a9e7a2aef
caofangkun <caofangkun@gmail.com>
2013-02-07 14:43:41 +0800

    Site updated at 2013-02-07 06:43:41 UTC

---
+++ javascripts/alimama.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"> 
+alimama_pid="mm_35248202_3473677_11317258"; 
+alimama_width=950; 
+alimama_height=90; 
+</script> 
+<script src="http://a.alimama.cn/inf.js" type="text/javascript"> 
+</script>
